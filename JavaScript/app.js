2c27962621ef5824130373e80e8385a8a76a404b
SAIFullah1212 <144945265+SAIFullah1212@users.noreply.github.com>
2023-09-14 02:50:36 +0500

    Add files via upload

---
+++ JavaScript/app.js
@@ -0,0 +1,180 @@
+// (Chapter 01)
+// (Q1)
+
+alert("First Name")
+alert("Last Name")
+alert("Email")
+alert("Phone Number")
+alert("Passward")
+
+// (Q2)
+
+alert("You're learning JavaScript!");
+
+// (Q3)
+
+alert("Hello, world! This is a custom message.                                                                                                                                                                                                                                                                                                                                                                                                  ");
+
+// (Chapter 02)
+// (Q1)
+
+let myVariableNameInCamelCase = "This is a variable declared in camelCase";
+
+console.log("The value of myVariableNameInCamelCase is:", myVariableNameInCamelCase);
+
+// (Q2)
+
+let myVariable;
+
+myVariable = "This is a string assigned to myVariable.";
+
+console.log("The value of myVariable is:", myVariable);
+
+// (Q3)
+
+let teamName = "MyTeam";
+
+alert("Our team name is: " + teamName);
+
+console.log("Our team name is:", teamName);
+
+// (Q4)
+
+var bestMan = "Charlie";
+
+bestMan = "David";
+
+console.log("The new value of bestMan is:", bestMan);
+
+// (Chapter 03)
+// (Q1 & Q2)
+
+let caseQty;
+caseQty = 144;
+console.log("The value of caseQty is:", caseQty);
+
+// (Q3)
+
+var num = 9;
+console.log("The value of num is:", num);
+
+// (Q4)
+
+let sum;
+sum = 5 + 7;
+console.log("The sum of 5 and 7 is:", sum);
+
+// (Q5)
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+console.log("The value of orderTotal is:", orderTotal);
+
+// (Q6)
+
+let myNumber = 10;
+myNumber = myNumber + 5;
+console.log("The updated value of myNumber is:", myNumber);
+
+// (Chapter 04)
+// (Q1)
+
+var productCost = 3.45;
+console.log("The product cost is:", productCost);
+
+// (Q2)
+
+var nameOfBand;
+console.log("The name of the band is:", nameOfBand);
+
+// (Q3)
+
+let Number = 42;
+console.log("The value of myNumber is:", Number);
+
+// (Q4)
+
+let myFullName = "FirstNameLastName";
+console.log("My full name is:", myFullName);
+
+// (Q5)
+
+let firstName = "John";
+let _lastName = "Doe";
+let middleName123 = "Alice";
+let camelCaseVariable = "Hello";
+let $currency = "USD";
+var name = "Invalid";
+let yourname = "Invalid";
+let myname = "Invalid";
+var name = "Invalid";
+
+console.log("Legal Variable Names:");
+console.log("let firstName =", firstName);
+console.log("let _lastName =", _lastName);
+console.log("let middleName123 =", middleName123);
+console.log("let camelCaseVariable =", camelCaseVariable);
+console.log("let $currency =", $currency);
+console.log("Illegal Variable Names:");
+
+// (Chapter 05)
+// (Q1)
+
+console.log("The name of the operator is: Modulo");
+console.log("The symbol for the operator is: %");
+
+// (Q2)
+
+var num = 20 % 6;
+console.log("The value of num is:", num);
+
+// (Q3)
+
+let largeNum = 1000 * 2000;
+console.log("The value of largeNum is:", largeNum);
+
+// (Q4)
+
+let num1 = 10;
+let num2 = 5;
+
+let result = num1 - num2;
+
+console.log("The result of subtracting num2 from num1 is:", result);
+
+// (Q5)
+
+let dividend = 15;
+let divisor = 4;
+
+let remainderResult = dividend % divisor;
+
+console.log("The remainder of dividing dividend by divisor is:", remainderResult);
+
+// (Q6)
+
+let num3 = 5;
+let num4 = 7;
+
+let res = num1 * num2;
+
+alert("The result of multiplying " + num1 + " and " + num2 + " is: " + result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
